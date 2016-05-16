912db1bf01a8e6a24d5e6f1fb01969a2ddee68a1
Eaiman Shoshi <coolshoshi.cse@gmail.com>
2016-05-17 02:05:42 +0600

    Initial commit with contributors

---
+++ src/com/chat/Config.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chat;
+
+/**
+ *
+ * @author Eaiman
+ */
+public class Config {
+    public static final String Q_TANNEE_SHOSHI = "Q_TANNEE_SHOSHI";
+    public static final String Q_TANNEE_NAHID = "Q_TANNEE_NAHID";
+    public static final String Q_TANNEE_ALL = "Q_TANNEE_ALL";
+    
+    public static final String Q_NAHID_SHOSHI = "Q_NAHID_SHOSHI";
+    public static final String Q_NAHID_TANNEE = "Q_NAHID_TANNEE";
+    public static final String Q_NAHID_ALL = "Q_NAHID_ALL";
+    
+    public static final String Q_SHOSHI_TANNEE = "Q_SHOSHI_TANNEE";
+    public static final String Q_SHOSHI_NAHID = "Q_SHOSHI_NAHID";
+    public static final String Q_SHOSHI_ALL = "Q_SHOSHI_ALL";
+    
+    public static final String E_ALL = "E_ALL";
+    public static final String E_TANNEE_SHOSHI = "E_TANNEE_SHOSHI";
+    public static final String E_NAHID_SHOSHI = "E_NAHID_SHOSHI";
+    public static final String E_TANNEE_NAHID = "E_TANNEE_NAHID";
+}
